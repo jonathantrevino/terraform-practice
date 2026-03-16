fa987ae845a9d148b6d610b2976478e38deed4b9
Jonathan Trevino <yeahimjt+github@gmail.com>
2026-03-16 00:46:49 -0500

    creating 1st ec2 instance tagged with name

---
+++ main.tf
@@ -0,0 +1,14 @@
+// specify aws provider + region
+provider "aws" {
+    region = "us-east-1"
+}
+
+// deploy a single ec2 instance
+resource "aws_instance" "example" {
+    ami             = "ami-40d28157"
+    instance_type   = "t2.micro"
+
+    tags = {
+        Name = "terraform-example"
+    }
+}
